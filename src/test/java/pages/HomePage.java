f594912f0ae3193efc16e0862bc4c93867604f63
IgorL <larkov039@ya.ru>
2017-10-26 21:11:59 +0300

    v2.12

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -13,21 +13,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HomePage {
-    private static final String RESOURCES_SUB_HEADER_LINKS_XPATH = "//div[@id='homepage-links']/ul/li/a";
-    private List<String> linkNames = Arrays.asList("Students", "Authors", "Instructors", "Librarians", "Societies", "Conferences",
-            "Booksellers", "Corporations", "Institutions");
-    private static final List LINK_NAMES = Arrays.asList("Home", "Subjects", "About Wiley", "Contact Us", "Help");
-    private static final String BUTTON_ELEMENT_XPATH = "//button[@id='id31']";
+    private static final String RESOURCES_SUB_HEADER_ELEMENTS_XPATH = "//div[@id='homepage-links']/ul/li/a";
+    private static final String RESOURCES_BAR_INSTITUTIONS_ELEMENT_XPATH = "//div[@id='homepage-links']/ul/li[@class='resource-institutions']";
+    private static final String SIGN_UP_TO_RECEIVE_SUBMIT_BUTTON_XPATH = "//button[@id='id31']";
     private static final String SIGN_UP_TO_RECEIVE_INPUT_ELEMENT_XPATH = "//input[@id='EmailAddress']";
     private static final String SEARCH_INPUT_ELEMENT_XPATH = "//input[@id='query']";
-    private static final String LOUPE_BUTTON_ELEMENT_XPATH = "//input[@class='icon icon__search search-form-submit']";
-    private static final String LIST_OF_ITEMS_XPATH = "//div[@id='search-results']/div[@class='product-listing size100']";
-    private static final String HEADER_XPATH = "//h1[@class='productDetail-title']";
-    private static final String ITEM_ELEMENT_LINK_XPATH = "div[@class='product-title']/a";
-    private static final String HOME_ELEMENT_XPATH = "//div[@id='links-site']/ul/li/a[contains(., 'Home')]";
-    private static final String INSTITUTIONS_ELEMENT_XPATH = "//div[@id='homepage-links']/ul/li[@class='resource-institutions']";
-
-
+    private static final String SEARCH_SUBMIT_BUTTON_XPATH = "//input[@class='icon icon__search search-form-submit']";
+    private static final String PRODUCT_LIST_OF_ITEMS_XPATH = "//div[@id='search-results']/div[@class='product-listing size100']";
+    private static final String PRODUCT_PAGE_HEADER_XPATH = "//h1[@class='productDetail-title']";
+    private static final String PRODUCT_ITEM_INFORMATION_LINK_XPATH = "div[@class='product-title']/a";
+    private static final String TOP_NAV_HOME_ELEMENT_XPATH = "//div[@id='links-site']/ul/li/a[contains(., 'Home')]";
+    private static final List TOP_NAV_LINK_NAMES = Arrays.asList("Home", "Subjects", "About Wiley", "Contact Us", "Help");
+    private static final List RESOURCES_BAR_ELEMENT_NAMES = Arrays.asList("Students", "Authors", "Instructors", "Librarians", "Societies", "Conferences",
+            "Booksellers", "Corporations", "Institutions");
     private final WebDriver driver;
 
     public HomePage(WebDriver driver) {
@@ -42,23 +40,15 @@ public class HomePage {
     }
 
     public HomePage checkTopMenuLinksHaveText() {
-        Assert.assertEquals(LINK_NAMES.size(), getLinksElements().size());
-        for (int linkIndex = 0; linkIndex < LINK_NAMES.size(); linkIndex++) {
-            Assert.assertEquals(LINK_NAMES.get(linkIndex), getLinksElements().get(linkIndex).getText());
+        Assert.assertEquals(TOP_NAV_LINK_NAMES.size(), getLinksElements().size());
+        for (int linkIndex = 0; linkIndex < TOP_NAV_LINK_NAMES.size(); linkIndex++) {
+            Assert.assertEquals(TOP_NAV_LINK_NAMES.get(linkIndex), getLinksElements().get(linkIndex).getText());
         }
         return this;
     }
 
-    private List<WebElement> getLinksElements() {
-        return driver.findElements(By.xpath("//div[@id='links-site']/ul/li/a"));
-    }
-
-    private List<WebElement> getResourcesSubHeaderLinks() {
-        return driver.findElements(By.xpath(RESOURCES_SUB_HEADER_LINKS_XPATH));
-    }
-
     public HomePage checkNumberOfResourcesSubHeaderLinks() {
-        checkNumberOfLinks(getResourcesSubHeaderLinks(), linkNames.size());
+        checkNumberOfLinks(getResourcesSubHeaderLinks(), RESOURCES_BAR_ELEMENT_NAMES.size());
         return this;
     }
 
@@ -76,20 +66,6 @@ public class HomePage {
         return this;
     }
 
-    private void checkNumberOfLinks(List<WebElement> links, int i) {
-        assertEquals("The number of links is equal " + links.size() + ", not " + i, i, links.size());
-    }
-
-    private void checkLinkHasText(WebElement link) {
-        assertTrue("Link text " + link.getText() + " is not contain among " +
-                        "titles 'Students', 'Authors', 'Instructors', 'Librarians', 'Societies', 'Conferences', 'Booksellers', 'Corporations', 'Institutions'",
-                linkNames.contains(link.getText()));
-    }
-
-    private void checkLinkIsDisplayed(WebElement link) {
-        assertTrue("Link " + link.getText() + " is not displayed", link.isDisplayed());
-    }
-
     public HomePage clickArrowButton() {
         getArrowButton().click();
         return this;
@@ -103,14 +79,6 @@ public class HomePage {
     public HomePage checkAlertTextMessage(String s) {
         assertEquals(s, getAlert().getText());
         return this;
-    }
-
-    private WebElement getArrowButton() {
-        return driver.findElement(By.xpath(BUTTON_ELEMENT_XPATH));
-    }
-
-    private Alert getAlert() {
-        return driver.switchTo().alert();
     }
 
     public HomePage enterTextIntoSignUpToReceiveInputElement() {
@@ -137,18 +105,6 @@ public class HomePage {
         return this;
     }
 
-    private WebElement getIntoSearchInputElement() {
-        return driver.findElement(By.xpath(SEARCH_INPUT_ELEMENT_XPATH));
-    }
-
-    private WebElement getLoupeButton() {
-        return driver.findElement(By.xpath(LOUPE_BUTTON_ELEMENT_XPATH));
-    }
-
-    private List<WebElement> getItemList() {
-        return driver.findElements(By.xpath(LIST_OF_ITEMS_XPATH));
-    }
-
     public HomePage checkRandomItemPageHeader() {
         WebElement randomItemLinkElement = getRandomItemLinkElement();
         String linkTxt = randomItemLinkElement.getText();
@@ -157,23 +113,9 @@ public class HomePage {
         return this;
     }
 
-    private WebElement getItemPageHeader() {
-        return driver.findElement(By.xpath(HEADER_XPATH));
-    }
-
-    private WebElement getRandomItemLinkElement() {
-        Random rn = new Random();
-        int rnd = rn.nextInt(getItemList().size());
-        return getItemList().get(rnd).findElement(By.xpath(ITEM_ELEMENT_LINK_XPATH));
-    }
-
     public HomePage clickHomeElement() {
         getHomeElement().click();
         return this;
-    }
-
-    private WebElement getHomeElement() {
-        return driver.findElement(By.xpath(HOME_ELEMENT_XPATH));
     }
 
     public HomePage clickInstitutionElement() {
@@ -195,9 +137,63 @@ public class HomePage {
         return this;
     }
 
-    private WebElement getInstitutionsElement() {
-        return driver.findElement(By.xpath(INSTITUTIONS_ELEMENT_XPATH));
+    private WebElement getIntoSearchInputElement() {
+        return driver.findElement(By.xpath(SEARCH_INPUT_ELEMENT_XPATH));
     }
 
+    private WebElement getLoupeButton() {
+        return driver.findElement(By.xpath(SEARCH_SUBMIT_BUTTON_XPATH));
+    }
 
+    private WebElement getItemPageHeader() {
+        return driver.findElement(By.xpath(PRODUCT_PAGE_HEADER_XPATH));
+    }
+
+    private WebElement getRandomItemLinkElement() {
+        Random rn = new Random();
+        int rnd = rn.nextInt(getItemList().size());
+        return getItemList().get(rnd).findElement(By.xpath(PRODUCT_ITEM_INFORMATION_LINK_XPATH));
+    }
+
+    private WebElement getHomeElement() {
+        return driver.findElement(By.xpath(TOP_NAV_HOME_ELEMENT_XPATH));
+    }
+
+    private WebElement getInstitutionsElement() {
+        return driver.findElement(By.xpath(RESOURCES_BAR_INSTITUTIONS_ELEMENT_XPATH));
+    }
+
+    private WebElement getArrowButton() {
+        return driver.findElement(By.xpath(SIGN_UP_TO_RECEIVE_SUBMIT_BUTTON_XPATH));
+    }
+
+    private List<WebElement> getItemList() {
+        return driver.findElements(By.xpath(PRODUCT_LIST_OF_ITEMS_XPATH));
+    }
+
+    private List<WebElement> getLinksElements() {
+        return driver.findElements(By.xpath("//div[@id='links-site']/ul/li/a"));
+    }
+
+    private List<WebElement> getResourcesSubHeaderLinks() {
+        return driver.findElements(By.xpath(RESOURCES_SUB_HEADER_ELEMENTS_XPATH));
+    }
+
+    private void checkNumberOfLinks(List<WebElement> links, int i) {
+        assertEquals("The number of links is equal " + links.size() + ", not " + i, i, links.size());
+    }
+
+    private void checkLinkHasText(WebElement link) {
+        assertTrue("Link text " + link.getText() + " is not contain among " +
+                        "titles 'Students', 'Authors', 'Instructors', 'Librarians', 'Societies', 'Conferences', 'Booksellers', 'Corporations', 'Institutions'",
+                RESOURCES_BAR_ELEMENT_NAMES.contains(link.getText()));
+    }
+
+    private void checkLinkIsDisplayed(WebElement link) {
+        assertTrue("Link " + link.getText() + " is not displayed", link.isDisplayed());
+    }
+
+    private Alert getAlert() {
+        return driver.switchTo().alert();
+    }
 }
--- src/test/java/pages/StudentSectionHomePage.java
+++ src/test/java/pages/StudentSectionHomePage.java
@@ -9,14 +9,12 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class StudentSectionHomePage {
-    private static final String STUDENTS_HEADER_XPATH = "//div[@id='page-title']/h1";
+    private static final String STUDENTS_PAGE_HEADER_XPATH = "//div[@id='page-title']/h1";
     private static final String RESOURCES_FOR_MENU_LINKS_XPATH = "//div[@id='sidebar']/div/ul/li/ul/li[@class='autonavItem']/a";
     private static final String RESOURCES_FOR_MENU_CLICKED_LINK_XPATH = "//div[@id='sidebar']/div/ul/li/ul/li/span";
     private static final String RESOURCES_FOR_MENU_ELEMENTS_XPATH = "//div[@id='sidebar']/div/ul/li/ul/li";
-    private static final String STUDENT_ELEMENT_XPATH = "//div[@id='sidebar']/div/ul/li/ul/li[@class='active autonavItem parent']/span";
-    private static final String HOME_ELEMENT_XPATH = "//div[@id='links-site']/ul/li/a[contains(., 'Home')]";
-
-
+    private static final String RESOURCES_FOR_MENU_STUDENT_LINK_XPATH = "//div[@id='sidebar']/div/ul/li/ul/li[@class='active autonavItem parent']/span";
+    private static final String TOP_NAV_HOME_ELEMENT_XPATH = "//div[@id='links-site']/ul/li/a[contains(., 'Home')]";
     private final WebDriver driver;
 
     public StudentSectionHomePage(WebDriver driver) {
@@ -33,24 +31,51 @@ public class StudentSectionHomePage {
         return this;
     }
 
-    private WebElement getStudentsHeader() {
-        return driver.findElement(By.xpath(STUDENTS_HEADER_XPATH));
-    }
-    private List<WebElement> getResourcesForMenuLinks(){
-        List<WebElement> resourcesForMenuLinks=driver.findElements(By.xpath(RESOURCES_FOR_MENU_LINKS_XPATH));
-        resourcesForMenuLinks.add(driver.findElement(By.xpath(RESOURCES_FOR_MENU_CLICKED_LINK_XPATH)));
-        return resourcesForMenuLinks;
-    }
-    public StudentSectionHomePage checkNumberOfResourcesForMenuLinks(){
+    public StudentSectionHomePage checkNumberOfResourcesForMenuLinks() {
         checkNumberOfLinks(getResourcesForMenuLinks(), 7);
         return this;
     }
-    public StudentSectionHomePage checkResourcesForMenuLinksAreDisplayed(){
+
+    public StudentSectionHomePage checkResourcesForMenuLinksAreDisplayed() {
         for (WebElement link : getResourcesForMenuLinks()) {
             checkItemIsDisplayed(link);
         }
         return this;
     }
+
+    public StudentSectionHomePage checkStudentElementStyle() {
+        assertFalse("'Students' item has the same style",
+                getResourcesForMenuElements().get(0).getCssValue("color").
+                        equals(getStudentElement().getCssValue("font-color")));
+        return this;
+    }
+
+    public StudentSectionHomePage checkStudentElementIsNotClickable() {
+        String pageUrl = driver.getCurrentUrl();
+        getStudentElement().click();
+        assertTrue("The click on 'Students' item changes page url", pageUrl.equals(driver.getCurrentUrl()));
+        return this;
+    }
+
+    public StudentSectionHomePage clickHomeElement() {
+        getHomeElement().click();
+        return this;
+    }
+
+    private WebElement getHomeElement() {
+        return driver.findElement(By.xpath(TOP_NAV_HOME_ELEMENT_XPATH));
+    }
+
+    private WebElement getStudentsHeader() {
+        return driver.findElement(By.xpath(STUDENTS_PAGE_HEADER_XPATH));
+    }
+
+    private List<WebElement> getResourcesForMenuLinks() {
+        List<WebElement> resourcesForMenuLinks = driver.findElements(By.xpath(RESOURCES_FOR_MENU_LINKS_XPATH));
+        resourcesForMenuLinks.add(driver.findElement(By.xpath(RESOURCES_FOR_MENU_CLICKED_LINK_XPATH)));
+        return resourcesForMenuLinks;
+    }
+
     private void checkNumberOfLinks(List<WebElement> links, int i) {
         assertEquals("The number of links is equal " + links.size() + ", not " + i, i, links.size());
     }
@@ -59,32 +84,11 @@ public class StudentSectionHomePage {
         assertTrue("Link " + link.getText() + " is not displayed", link.isDisplayed());
     }
 
-    public StudentSectionHomePage checkStudentElementStyle(){
-        assertFalse("'Students' item has the same style",
-                getResourcesForMenuElements().get(0).getCssValue("color").
-                        equals(getStudentElement().getCssValue("font-color")));
-        return this;
-    }
-    public StudentSectionHomePage checkStudentElementIsNotClickable(){
-        String pageUrl = driver.getCurrentUrl();
-        getStudentElement().click();
-        assertTrue("The click on 'Students' item changes page url", pageUrl.equals(driver.getCurrentUrl()));
-        return this;
-    }
-    private List<WebElement> getResourcesForMenuElements(){
+    private List<WebElement> getResourcesForMenuElements() {
         return driver.findElements(By.xpath(RESOURCES_FOR_MENU_ELEMENTS_XPATH));
     }
-    private WebElement getStudentElement(){
-        return driver.findElement(By.xpath(STUDENT_ELEMENT_XPATH));
-    }
-    public StudentSectionHomePage clickHomeElement(){
-        getHomeElement().click();
-        return this;
-    }
 
-    private WebElement getHomeElement() {
-        return driver.findElement(By.xpath(HOME_ELEMENT_XPATH));
+    private WebElement getStudentElement() {
+        return driver.findElement(By.xpath(RESOURCES_FOR_MENU_STUDENT_LINK_XPATH));
     }
-
-
 }
